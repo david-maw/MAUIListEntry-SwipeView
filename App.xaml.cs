48a57b31aa63e2728dbd237b3513dcbeb21d58b0
David Maw <david-maw@users.noreply.github.com>
2023-11-05 17:47:54 -0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MAUIListEntry;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        // Set the App window to a sensible (phone like) size during initialization
+        if (DeviceInfo.Idiom == DeviceIdiom.Desktop)
+        {
+            window.Height = 1200;
+            window.Width = 600;
+        }
+
+        return window;
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MAUIListEntry;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        for (int i = 0; i < 200; i++)
+        {
+            Items.Add("Item " + i);
+        }
+	}
+    public List<String> Items { get; private set; } = new List<String>();
+ }
+
